430b2ae7b61d26a9812d2cc43045ae09095979ec
shahazad08 <sk.shahazad@gmail.com>
2025-10-06 12:35:28 +0530

    Added: Addition Functionality added

--- src/com/theemOne/HelloWorld.java
+++ src/com/theemOne/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Theem World!");
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("Addition is"+c);
 		// TODO Auto-generated method stub
 
 	}
