f3c72da060e77ad62c6038d559f5a47d8e133fcc
shahazad08 <sk.shahazad@gmail.com>
2025-10-06 12:28:20 +0530

    Added: Hello World Project is addeed

---
+++ src/com/theemOne/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.theemOne;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Theem World!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
